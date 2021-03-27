78654cbde9813ad5c084c5a8fde827983f725773
Ad1tyaV <adithya.vadrevu@gmail.com>
2021-03-27 01:58:52 -0400

    Update randomRun.js

--- helper/randomRun.js
+++ helper/randomRun.js
@@ -34,7 +34,7 @@ const RandomWithIndex=(batterIndex)=>{
             //console.log(arr[index_rc])
             return arr[index_rc];
         }
-        case 1:{
+        case 0:{
             freq=[3,70,45,14,0,12,2]//143 balls Shikhar 133
             prefix = freq.map(cumulativeSum)
             //console.log(prefix)
@@ -45,7 +45,7 @@ const RandomWithIndex=(batterIndex)=>{
             //console.log(arr[index_rc])
             return arr[index_rc];
         }
-        case 2:{
+        case 1:{
             freq=[3,91,50,16,1,17,2]//Kohli 177 balls 165
             prefix = freq.map(cumulativeSum)
             let random = Math.floor(Math.random()*prefix[prefix.length-1])
@@ -53,7 +53,7 @@ const RandomWithIndex=(batterIndex)=>{
             //console.log(arr[index_rc])
             return arr[index_rc];
         }
-        case 3:{
+        case 2:{
             freq=[4,80,45,16,1,10,5]//Shreyas 157 balls 150
             prefix = freq.map(cumulativeSum)
             let random = Math.floor(Math.random()*prefix[prefix.length-1])
@@ -61,7 +61,7 @@ const RandomWithIndex=(batterIndex)=>{
             //console.log(arr[index_rc])
             return arr[index_rc];
         }
-        case 4:{
+        case 3:{
             freq=[5,91,45,7,1,18,6]//Pant 149 balls 178
             prefix = freq.map(cumulativeSum)
             let random = Math.floor(Math.random()*prefix[prefix.length-1])
@@ -69,7 +69,7 @@ const RandomWithIndex=(batterIndex)=>{
             //console.log(arr[index_rc])
             return arr[index_rc];
         }
-        case 5:{
+        case 4:{
             freq=[5,91,40,5,1,12,7]//KL R 160 balls 181
             prefix = freq.map(cumulativeSum)
             let random = Math.floor(Math.random()*prefix[prefix.length-1])
@@ -77,7 +77,7 @@ const RandomWithIndex=(batterIndex)=>{
             //console.log(arr[index_rc])
             return arr[index_rc];
         }
-        case 6:{
+        case 5:{
             freq=[6,80,38,5,1,15,8]//Pandya 158 balls 181
             prefix = freq.map(cumulativeSum)
             let random = Math.floor(Math.random()*prefix[prefix.length-1])
@@ -85,7 +85,7 @@ const RandomWithIndex=(batterIndex)=>{
             //console.log(arr[index_rc])
             return arr[index_rc];
         }
-        case 7:{
+        case 6:{
             freq=[6,80,45,16,1,18,4]//Jadeja 165 balls 176
             prefix = freq.map(cumulativeSum)
             let random = Math.floor(Math.random()*prefix[prefix.length-1])
@@ -101,8 +101,16 @@ const RandomWithIndex=(batterIndex)=>{
             //console.log(arr[index_rc])
             return arr[index_rc];
         }
-        case 9:{
+        case 7:{
             freq=[9,100,25,15,1,18,2]//Bhuvi 160 balls 142
+            prefix = freq.map(cumulativeSum)
+            let random = Math.floor(Math.random()*prefix[prefix.length-1])
+            let index_rc=getCeilValue(prefix,random)
+            //console.log(arr[index_rc])
+            return arr[index_rc];
+        }
+        case 9:{
+            freq=[9,100,25,5,1,18,2]
             prefix = freq.map(cumulativeSum)
             let random = Math.floor(Math.random()*prefix[prefix.length-1])
             let index_rc=getCeilValue(prefix,random)
